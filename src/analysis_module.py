608a5509bc2bd99e3ffff118255e165bdad78dff
yangji73 <yangji73@cmse036.ad.egr.msu.edu>
2021-04-22 18:06:42 -0400

    add analysis function

--- src/analysis_module.py
+++ src/analysis_module.py
@@ -3,7 +3,7 @@
 '''
 Author: Jiaxin Yang
 Date: 2021-04-22 13:52:51
-LastEditTime: 2021-04-22 16:32:57
+LastEditTime: 2021-04-22 18:06:32
 LastEditors: Jiaxin Yang
 Description: 
 FilePath: \undefinedd:\Program Files (x86)\vs_python\VR_analysis\src\analysis_module.py
@@ -22,6 +22,11 @@ def spatial_hub(sturcture, sites):
             sites:      a specific binding sites, protein, DNase, gv
     @return:
     '''
+    # cumulative counts
+    # counts difference matrix
+    # density matrix
+    # find out
+
     return output
 
 
@@ -33,17 +38,23 @@ def interaction_hub(sturcture, links):
             links:      interaction data, eQTL
     @return:
     '''
+    
     return output
 
 
-def loop_3d(structure, distance):
+def loop_3d(structure, size, threshold):
     '''
     @description: get the chromatin loops 
     @param:
             structure:  3D structurs
-            distance:   distance threshold
+            size:       size of loops
+            threshold:  distance threshold
     @return:
     '''
+    # calcualte pairwise distance
+    # find out the points with small 3D distance
+    # filter the size
+
     return output
 
 
@@ -55,4 +66,5 @@ def neighbor_3D(structure, distance):
             distance:   distance threshold
     @return:
     '''
+    # seems similar with loop
     return output
