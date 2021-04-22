5abd386cd9105fcc104a219de43bd6e3a7a45d6b
yangji73 <yangji73@cmse036.ad.egr.msu.edu>
2021-04-22 16:33:45 -0400

    add analysis function

---
+++ src/analysis_module.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Author: Jiaxin Yang
+Date: 2021-04-22 13:52:51
+LastEditTime: 2021-04-22 16:32:57
+LastEditors: Jiaxin Yang
+Description: 
+FilePath: \undefinedd:\Program Files (x86)\vs_python\VR_analysis\src\analysis_module.py
+'''
+
+import pandas as pd
+import numpy as np
+
+
+
+def spatial_hub(sturcture, sites):
+    '''
+    @description: generate 3D spatial hubs of specific sites
+    @param:
+            structure:  3D structurs
+            sites:      a specific binding sites, protein, DNase, gv
+    @return:
+    '''
+    return output
+
+
+def interaction_hub(sturcture, links):
+    '''
+    @description: generate hubs with high degree of interaction
+    @param:
+            structure:  3D structurs
+            links:      interaction data, eQTL
+    @return:
+    '''
+    return output
+
+
+def loop_3d(structure, distance):
+    '''
+    @description: get the chromatin loops 
+    @param:
+            structure:  3D structurs
+            distance:   distance threshold
+    @return:
+    '''
+    return output
+
+
+def neighbor_3D(structure, distance):
+    '''
+    @description: get the closes module
+    @param:
+            structure:  3D structurs
+            distance:   distance threshold
+    @return:
+    '''
+    return output
