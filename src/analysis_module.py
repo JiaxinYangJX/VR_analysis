1546d0a7f61f8001c02ba7fcd0015628c69f870e
yangji73 <yangji73@cmse036.ad.egr.msu.edu>
2021-05-03 17:41:49 -0400

    add VR analysis function

--- src/analysis_module.py
+++ src/analysis_module.py
@@ -3,7 +3,7 @@
 '''
 Author: Jiaxin Yang
 Date: 2021-04-22 13:52:51
-LastEditTime: 2021-04-23 11:43:20
+LastEditTime: 2021-05-03 17:40:20
 LastEditors: Jiaxin Yang
 Description: 
 FilePath: \undefinedd:\Program Files (x86)\vs_python\VR_analysis\src\analysis_module.py
@@ -12,64 +12,230 @@ FilePath: \undefinedd:\Program Files (x86)\vs_python\VR_analysis\src\analysis_mo
 import pandas as pd
 import numpy as np
 from helper import *
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from collections import Counter
 
 
-def spatial_hub(sturcture, sites):
+
+def read_structure(xyz_path, frag_id_path):
     '''
+    @description: read structure
+
+    @param:
+            xyz_path:       path to xyz coord
+            frag_id_path:   path to fragment 1D       
+    @return:
+            frag:   numpy array, fragment 1D position
+            xyz:    numpy array, xyz
+    '''    
+    xyz     = pd.read_csv(xyz_path, header=None, sep='\t')
+    frag_id = pd.read_csv(frag_id_path, header=None, sep='\t')
+    
+    xyz   = xyz.iloc[:,1:].values
+    start = frag_id.iloc[:,0].values
+    start = np.expand_dims(start, axis=1) 
+    end   = start + 5000
+    frag  = np.concatenate([start, end], axis=1)
+
+    return frag, xyz
+
+
+def read_sites(sites_path, chr_id):
+    '''
+    @description: read binding sites dataset
+
+    @param:
+            sites_path: path to the binding sites data
+            chr_id:     chr id
+    @return:
+            sites: numpy array, binding sites
+    '''
+    sites = pd.read_csv(sites_path, header=None, sep='\t')
+    sites = sites[sites.iloc[:,0]==chr_id].iloc[:,1:3].values
+
+    return sites 
+
+
+def read_links(links_path, chr_id):
+    '''
+    @description: read chromatin interaction dataset
+
+    @param:
+            links_path: path to the links: chr, frag1_start, end, frag2_start, end
+            chr_id:     chr id
+    @return:
+            links:      numpy array, fragment links
+    '''
+    links = pd.read_csv(links_path, header=None, sep='\t')
+    links = links[links.iloc[:,0]==chr_id].iloc[:,1:5].values
+    return links
+
+
+'''
+def spatial_hub(frag, xyz, sites, cluster_size=40):
+    ''
     @description: generate 3D spatial hubs of specific sites
     @param:
-            structure:  3D structurs
+            structure:  3D structurs: start end x y z
             sites:      a specific binding sites, protein, DNase, gv
     @return:
-    '''
+    ''
     # map the sites into 3D structure
-    dense_3D = sites_map(sturcture, sites)
+    sites_coord, sites_id = sites_map(frag, xyz, sites)
+    
+    # k-means clustering
+    ncluster = len(sites_coord) // cluster_size
+    my_kmeans = KMeans(n_clusters=ncluster).fit(sites_coord)
+    
+    # filter out the broad clusers
+    for i in range(ncluster):
+        dist = sites_coord[my_kmeans.labels_==i] - my_kmeans.cluster_centers_[i]
+        print(np.sum(np.sum(dist**2, axis=1) < 10))
 
-    # network construct, link the closed fragment adj network
-    adj_matrix = network_construct(sturcture) 
 
-    # detact community
 
     return output
+'''
+
+def spatial_hub_hiera(frag, xyz, sites, dist_thres=3, cluster_size_thres=0.5):
+    '''
+    @description: generate 3D spatial hubs of specific sites
+
+    @param:
+            frag:       frag_id
+            xyz:        xyz
+            sites:      a specific binding sites, protein, DNase, gv
+            dist_thres: distance threshold in hierachial clustering
+            cluster_size_thres: only clusters with top 0.5 sizes
+    @return:
+            group_list: list, contains the frag_id in each 3D hub
+    '''
+    # 1. map the sites into 3D structure
+    sites_coord, sites_id = sites_map(frag, xyz, sites)
+    
+    # 2. hierachical cluster
+    my_hiera = AgglomerativeClustering(distance_threshold = dist_thres).fit(sites_coord)
+
+    # 3. only keep the cluster with enough fragments, default: top 50%
+    cluster_counter = Counter(my_hiera.labels_)
+    size_thres = np.quantile(np.array(list(cluster_counter.items()))[:,1],q=cluster_size_thres)
+    group_list = []
+    for label, count in cluster_counter.most_common():
+        if count > size_thres:
+            group_list.append(sites_coord[my_hiera.labels_==label,])
+ 
+    return group_list
 
 
-def interaction_hub(sturcture, links):
+
+
+def interaction_hub(frag, xyz, links, q_quantile = 0.99):
     '''
     @description: generate hubs with high degree of interaction
+
     @param:
-            structure:  3D structurs
-            links:      interaction data, eQTL
+            frag:       frag_id
+            xyz:        xyz
+            links:      frag-frag links
+            q_quantile: top 0.99 dense degree
     @return:
+            group_list: numpy array, contains the start and end id of hubs
     '''
-    # think about airport
-    # 
+    # 1. links to 2 regions
+    region_1 = links[:,0:2]
+    region_2 = links[:,2:4]
+    region   = np.concatenate([region_1, region_2], axis=0)
+
+    # 2. map to 1d, get degree
+    frag_degree = degree_map(frag, region)
+
+    # 3. cumulative increase
+    cum_degree = np.cumsum(frag_degree)
+
+    # 4. find the dense 1D region
+    size = 5
+    degree_list = []
+    for p in range(frag.shape[0]-size):
+        degree_list.append(cum_degree[p+size]-cum_degree[p])
+    degree_list = np.array(degree_list)
     
-    return output
+    # find the high degree regions
+    thres = np.quantile(degree_list, q = q_quantile)
+    high_region_start = np.where(degree_list > thres)[0] # high range: (p,p+size]
+    idx = 0
+    start_idx = high_region_start[0] + 1 # [p+1,p+size]
+    # merge the region
+    group_list = []
+    while idx < len(high_region_start)-1:
+        if (high_region_start[idx] + size) >= high_region_start[idx+1]:
+            # overlap
+            idx += 1
+        else: # save
+            group_list.append([start_idx, high_region_start[idx]+size])
+            start_idx = high_region_start[idx+1] + 1
+            idx += 1
+    group_list.append([start_idx, high_region_start[idx]+size]) # add last
+    
+    return np.array(group_list)
 
 
-def loop_3d(structure, size, threshold):
+
+def loop_3d(frag, xyz, scale = 100000, resolution = 5000, q_quantile=0.002):
     '''
-    @description: get the chromatin loops 
+    @description: get the chromatin loops
+
     @param:
-            structure:  3D structurs
-            size:       size of loops
-            threshold:  distance threshold
+            frag:       frag_id
+            xyz:        xyz
+            scale:      loop scale
+            resolution: resolution of the structure
+            q_quantile: top 0.002 closest 
     @return:
+            loop_list:  numpy array, contains the start and end id of loops
     '''
-    # calcualte pairwise distance
-    # find out the points with small 3D distance
-    # filter the size
+    # 1. find the 1) distance between two fragment
+    size = scale // resolution
+    dist_list = []
+    for p in range(frag.shape[0]-size+1):
+        dist_tmp = np.linalg.norm(xyz[p] - xyz[p+size-1])
+        dist_list.append(dist_tmp)
+    dist_list = np.array(dist_list)
 
-    return output
+    # 2. find the loop 
+    thres = np.quantile(dist_list, q = q_quantile)
+    close_loop_start = np.where(dist_list < thres)[0] # range: [p,p+size]
+    
+    # 3. merge the loop
+    idx = 0
+    start_idx = close_loop_start[0] 
+    loop_list = []
+    while idx < len(close_loop_start)-1:
+        if (close_loop_start[idx] + size) >= close_loop_start[idx+1]:
+            # overlap
+            idx += 1
+        else: # save
+            loop_list.append([start_idx, start_idx + size])
+            start_idx = close_loop_start[idx+1]
+            idx += 1
+    loop_list.append([start_idx, start_idx+size]) # add last
+    
+    return np.array(loop_list)
 
 
-def neighbor_3D(structure, distance):
-    '''
-    @description: get the closes module
-    @param:
-            structure:  3D structurs
-            distance:   distance threshold
-    @return:
-    '''
-    # seems similar with loop
-    return output
+def main():
+    xyz_path     = 'structure/chr1_1502144569709.xyz.txt'
+    frag_id_path = 'structure/chr1_coordinate_mapping.txt'
+    sites_path   = 'binding/ENCSR000EMT_rep2_1_se_bwa_biorep_filtered_peaks.bed'
+    links_path   = 'links/GM_link.txt'
+    chr_id       = 'chr1'
+
+    hub_3d = spatial_hub_hiera(frag, xyz, sites)
+
+    inter_hub = interaction_hub(frag, xyz, links)
+
+    loop = loop_3d(frag, xyz)
+
+    return None
+
+if __name__ == "__main__":
+    main() 
--- src/helper.py
+++ src/helper.py
@@ -1,8 +1,75 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 '''
 Author: Jiaxin Yang
 Date: 2021-04-23 11:42:29
-LastEditTime: 2021-04-23 11:42:29
+LastEditTime: 2021-05-03 17:37:35
 LastEditors: Jiaxin Yang
 Description: 
 FilePath: \undefinedd:\Program Files (x86)\vs_python\VR_analysis\src\helper.py
 '''
+
+import numpy as np
+
+def sites_map(frag, xyz, sites):
+    '''
+    @description: map the binding sites to the 3D fragment
+
+    @param:
+            frag:   start, end; sorted
+            xyz :   x, y, z
+            sites:  position
+    @return:
+            sites_coord:    numpy array, coord of each site
+            sites_id:       list, id of each site
+    '''
+    frag_idx = 0
+    site_idx = 0
+    sites_coord = []
+    sites_id = []
+
+    # sites center, sort
+    sites_center = (sites[:,1] + sites[:,0]) // 2
+    sites_center = np.sort(sites_center)
+
+    while (frag_idx < len(frag)) and (site_idx < len(sites)):
+        if sites_center[site_idx] <= frag[frag_idx,0]:
+            site_idx += 1
+        elif sites_center[site_idx] <= frag[frag_idx,1]:
+            sites_coord.append(xyz[frag_idx])
+            sites_id.append(frag_idx)
+            site_idx += 1
+        else:
+            frag_idx += 1
+
+    return np.array(sites_coord), sites_id
+
+
+def degree_map(frag, region):
+    '''
+    @description: map the regions to 1D, get 1D fragment degrees
+
+    @param:
+            frag:   start, end; sorted
+            region: position
+    @return:
+            frag_degree: numpy array, degree of each fragment
+    '''
+    frag_idx    = 0
+    region_idx  = 0
+    frag_degree = np.zeros(len(frag))
+
+    # region center, sort
+    region_center = (region[:,1] + region[:,0]) // 2
+    region_center = np.sort(region_center)
+
+    while (frag_idx < len(frag)) and (region_idx < len(region_center)):
+        if region_center[region_idx] <= frag[frag_idx,0]:
+            region_idx += 1
+        elif region_center[region_idx] <= frag[frag_idx,1]:
+            frag_degree[frag_idx] += 1
+            region_idx += 1
+        else:
+            frag_idx += 1
+
+    return frag_degree
